5e57420f1bc2f4f125a4cfac72a8f24ef5f0997e
DJFancyAl <abluemleiv@gmail.com>
2024-02-24 14:48:32 -0500

    Created Exercises 5 & 6

--- 5_make_bank_bro.py
+++ 5_make_bank_bro.py
@@ -1,6 +1,70 @@
 '''
-Get ass.
 
-PAHHHHH
+THERE'S A CRISIS! We need to fund a new "LGBT+ program" for new migrants who are arriving from the southern border.
+And money is tight because we already paid for the LGBT center in Pakistan last year, so we're gonna need the tax
+payers to chip in a little more. DEMOCRACY IS AT STAKE.
+
+However, it's also election season and we can't risk upsetting our educated class. So our lord and savior (Boe Jiden)
+has asked for a list of the poorest Americans at his bank.
+
+Take a look at the list below. You need to use this list to determine the balance of each bank account. Then you need
+to add the account number of each account totaling less than $6000.00 to the "poor_people" list. Lastly, print the list.
+
+Hints:
+* Look carefully at how the bank_accounts list is structured to determine how to iterate it.
+* There are several ways to accomplish this, but some are more logical than others.
+* Remember - your result needs to only be a list of account numbers.
 
 '''
+
+bank_accounts = [
+       {
+           "account": 49358102,
+           "transactions": [-3824.71, 2319.05, 4127.98, -1205.36, 380.27, -456.89, 321.04, 1902.57, -742.13, 4809.21]  
+       },
+       {
+           "account": 71602395,
+           "transactions": [2871.42, -1739.51, 3405.29, -4028.16, 1029.38, -2104.72, 1832.09, 4907.15, -3210.84, 2017.63, 789.25, -4123.98, 1350.72, -2981.06, 4529.31] 
+       },
+       {
+           "account": 82641709,
+           "transactions": [4231.87, -3102.49, 1928.56, -874.12, 2039.74, -4810.15, 1209.32, 3571.98, -2042.71, 4920.34, -1305.87, 728.41, -3892.53, 2107.94, -1820.65, 3219.08, -982.31, 4012.79, -2738.14, 1567.02] 
+       },
+       {
+           "account": 75213068,
+           "transactions": [3482.17, -1209.83, 2710.45, -4321.07, 839.12, -3987.26, 1456.82, 4102.93, -2519.34, 3081.56] 
+       },
+       {
+           "account": 13509246,
+           "transactions": [1520.94, 3960.08, -452.18, -1051.49, 4188.06, 3209.33, 2234.04, -1765.90, 4574.75, 709.28, -4021.86, -570.02]
+       },
+       {
+           "account": 28074615,
+           "transactions": [-3719.28, 820.14, 2107.46, -4901.52, 1930.71, -1289.34, 4028.19, -2509.87, 3921.04, -784.21, 1002.39, -3018.56, 2731.09, 4812.74, -1507.23]
+       },
+       {
+           "account": 96420531,
+           "transactions": [4071.29, -1839.05, 3102.94, -4208.76, 2729.83, -3047.19, 1402.05, 4517.61, -2910.34, 3072.13, 1298.75, -4823.59, 2850.27, -1090.63, 3901.28, -2379.16, 4732.09, -3218.94, 1820.51, 2569.32]
+       },
+       {
+           "account": 51387240,
+           "transactions": [-1721.38, 2094.27, 3829.51, -418.72, 1307.94, -3210.87, 2901.23, 4018.65, -2309.14, 4231.02, -874.56, 1529.38, -3572.19, 2607.42, -1928.53, 3419.08, -2102.45, 1039.71]
+       },
+       {
+           "account": 27095638,
+           "transactions": [-3414.78, -585.86, 4845.18, 633.42, 844.61, -820.87, -2830.91, -2796.67, -2244.11]
+       },
+       {
+           "account": 64951327,
+           "transactions": [4321.54, -2910.83, 3420.17, -745.29, 1528.64, -1093.72, 2839.15, -4920.31, 1234.87, -3012.94, 3821.06, -2187.53, 4098.27]
+       }
+    ]
+
+poor_people = []
+
+# Write your code below this line.
+
+
+
+# And above this line
+print(poor_people)
